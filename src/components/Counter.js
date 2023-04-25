a248165bf9ef4223d01922226a89bafff7085064
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-25 21:53:24 +0300

    first commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,13 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log(`before setState: ${count}`);
+
     setCount(count + 1);
+
+    console.log(`after setState: ${count}`);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <div onClick={increment}>{count}</div>;
 }
-
 export default Counter;
